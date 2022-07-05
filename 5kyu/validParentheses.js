06fea6402eba4b4b861907bf2f87a6036514c6d1
Wojciech Chuchro <wojciech.chuchro@outlook.com>
2022-07-05 13:36:47 +0200

    do 5kyu valid parentheses challange

---
+++ 5kyu/validParentheses.js
@@ -0,0 +1,22 @@
+//Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+//
+// Examples
+// "()"              =>  true
+// ")(()))"          =>  false
+// "("               =>  false
+// "(())((()())())"  =>  true
+
+//My solution
+
+function validParentheses(parens) {
+    let indent = 0;
+
+    for (let i = 0; i < parens.length && indent >= 0; i++) {
+        indent += (parens[i] === '(') ? 1 : -1;
+    }
+
+    return (indent === 0);
+}
+
+console.log('true: ' + validParentheses('())')) //true
+console.log('false: ' + validParentheses(')(()))')) //false
